a279a100c82023c322b5e68d45d9a242c3116f1c
KazimierczakJudyta <kazimierczakjudyta@gmail.com>
2022-09-15 23:00:12 +0200

    The JS code was moved into the file located in src folder

--- src/app.js
+++ src/app.js
@@ -1,39 +1,3 @@
-function search(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector(".searchbar").value;
-  let apiKey = "ae83a60794fa4d45d88e6b19756a1473";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-}
-
-function displayWeather(response) {
-  console.log(response);
-  document.querySelector(".city-name").innerHTML = response.data.name;
-  document.querySelector(".temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-}
-function searchLocation(position) {
-  let apiKey = "ae83a60794fa4d45d88e6b19756a1473";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeather);
-  console.log(apiUrl);
-}
-function currentWeather(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-function changeToCelsius(event) {
-  event.preventDefault();
-  let dayTemperature = document.querySelector(".temperature");
-  dayTemperature.innerHTML = "20 C°";
-}
-function changeToFahrenheit(event) {
-  event.preventDefault();
-  let dayTemperature = document.querySelector(".temperature");
-  dayTemperature.innerHTML = "20 °F";
-}
-
 let now = new Date();
 now.getDay();
 let hours = now.getHours();
@@ -73,15 +37,68 @@ date.innerHTML = `${nameDay} ${month} ${year}`;
 let time = document.querySelector(".time");
 time.innerHTML = `${hours}:${minutes}`;
 
-let lookUp = document.querySelector(".searchbar");
+function displayTemperature(response) {
+  console.log(response);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city-name");
+  let cloudElement = document.querySelector("#cloudy");
+  let weatherInfo = document.querySelector("#weather-info");
+  let fellsLike = document.querySelector("#feels");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let pressureElement = document.querySelector("#pressure");
+  let iconElement = document.querySelector("#icon");
+  celsiusTemperature = Math.round(response.data.main.temp);
+  celsius.style.color = "#101510";
+  temperatureElement.innerHTML = celsiusTemperature;
+  cityElement.innerHTML = response.data.name;
+  cloudElement.innerHTML = response.data.clouds.all;
+  weatherInfo.innerHTML = response.data.weather[0].description;
+  fellsLike.innerHTML = Math.round(response.data.main.feels_like);
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = response.data.wind.speed;
+  pressureElement.innerHTML = response.data.main.pressure;
+  iconElement.setAttribute(
+    "src",
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`
+  );
+}
+function search(city) {
+  let apiKey = "ae83a60794fa4d45d88e6b19756a1473";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleData(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#searchbar");
+  search(cityInputElement.value);
+}
 
-lookUp.addEventListener("submit", search);
+function showFahrenheit(event) {
+  event.preventDefault();
+  let fahrenheitTemperature = celsiusTemperature * 1.8 + 32;
+  let temperature = document.querySelector("#temperature");
+  let fahrenheit = document.querySelector("#fahrenheit");
+  temperature.innerHTML = fahrenheitTemperature;
+  fahrenheit.style.color = "#101510";
+  celsius.style.color = "#2c3a2b";
+}
+function showCelsius(event) {
+  event.preventDefault();
+  let temperature = document.querySelector("#temperature");
+  temperature.innerHTML = celsiusTemperature;
+  celsius.style.color = "#101510";
+  fahrenheit.style.color = "#2c3a2b";
+}
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", showFahrenheit);
 
-let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click", changeToCelsius);
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", showCelsius);
 
-let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", changeToFahrenheit);
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleData);
 
-let current = document.querySelector("#search-form");
-current.addEventListener("click", currentWeather);
+let celsiusTemperature = null;
+
+search("New York");
