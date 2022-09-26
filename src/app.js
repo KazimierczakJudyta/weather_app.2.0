81080bbe46acf0d084d0ae2a0f1b3075b2b7e6a8
KazimierczakJudyta <kazimierczakjudyta@gmail.com>
2022-09-26 22:01:16 +0200

    API key update

--- src/app.js
+++ src/app.js
@@ -39,7 +39,7 @@ time.innerHTML = `${hours}:${minutes}`;
 
 function getForecast(coordinates) {
   console.log(coordinates);
-  let apiKey = "ae83a60794fa4d45d88e6b19756a1473";
+  let apiKey = "06a9329363b3c8a22d8390d3092184eb";
   let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
@@ -70,7 +70,7 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "ae83a60794fa4d45d88e6b19756a1473";
+  let apiKey = "06a9329363b3c8a22d8390d3092184eb";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
